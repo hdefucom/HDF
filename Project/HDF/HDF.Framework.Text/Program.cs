e1a302d9936162dbcceec9ea0ea8dcf99aa4b385
德芙 <1213159982@qq.com>
2019-12-19 11:27:47 +0800

    a

---
+++ Project/HDF/HDF.Framework.Text/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDF.Framework.Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //进程启动
+            {
+                //Task.Run(() =>
+                //{
+                //    Form_CallProgram form = new Form_CallProgram();
+                //    form.ShowDialog();
+                //});
+            }
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
