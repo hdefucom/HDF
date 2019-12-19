542103dd24192325ad1d53400c1fd5e7123d5824
德芙 <1213159982@qq.com>
2019-12-19 11:00:07 +0800

    添加项目

---
+++ Project/HDF/HDF.Framework.Common/CompressHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace HDF.Framework.Common
+{
+    /// <summary>
+    /// 压缩帮助类
+    /// </summary>
+    public static class CompressHelper
+    {
+        /// <summary>
+        /// 将传入字符串以GZip算法压缩后，返回Base64编码字符
+        /// </summary>
+        /// <param name="rawString">需要压缩的字符串</param>
+        /// <returns>压缩后的Base64编码的字符串</returns>
+        public static string GZipCompressString(this string rawString)
+        {
+            if (string.IsNullOrEmpty(rawString) || rawString.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                byte[] rawData = Encoding.UTF8.GetBytes(rawString.ToString());
+                return (Convert.ToBase64String(Compress(rawData)));
+            }
+
+        }
+
+        /// <summary>
+        /// 将传入的二进制字符串资料以GZip算法解压缩
+        /// </summary>
+        /// <param name="zippedString">经GZip压缩后的二进制字符串</param>
+        /// <returns>原始未压缩字符串</returns>
+        public static string GZipDecompressString(this string zippedString)
+        {
+            if (string.IsNullOrEmpty(zippedString) || zippedString.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                byte[] zippedData = Convert.FromBase64String(zippedString.ToString());
+                return (Encoding.UTF8.GetString(Decompress(zippedData)));
+            }
+        }
+
+        /// <summary>
+        /// GZip压缩
+        /// </summary>
+        /// <param name="rawData"></param>
+        /// <returns></returns>
+        public static byte[] Compress(byte[] rawData)
+        {
+            MemoryStream ms = new MemoryStream();
+            GZipStream compressedzipStream = new GZipStream(ms, CompressionMode.Compress, true);
+            compressedzipStream.Write(rawData, 0, rawData.Length);
+            compressedzipStream.Close();
+            return ms.ToArray();
+        }
+
+        /// <summary>
+        /// GZIP解压
+        /// </summary>
+        /// <param name="zippedData"></param>
+        /// <returns></returns>
+        public static byte[] Decompress(byte[] zippedData)
+        {
+            MemoryStream ms = new MemoryStream(zippedData);
+            GZipStream compressedzipStream = new GZipStream(ms, CompressionMode.Decompress);
+            MemoryStream outBuffer = new MemoryStream();
+            byte[] block = new byte[1024];
+            while (true)
+            {
+                int bytesRead = compressedzipStream.Read(block, 0, block.Length);
+                if (bytesRead <= 0)
+                    break;
+                else
+                    outBuffer.Write(block, 0, bytesRead);
+            }
+            compressedzipStream.Close();
+            return outBuffer.ToArray();
+        }
+
+
+    }
+}
---
+++ Project/HDF/HDF.Framework.Common/DBHelper.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace HDF.Framework.Common
+{
+    #region 非静态抽象继承
+
+    /*
+    public abstract class DBHelper
+    {
+        public DBHelper(string key)
+        {
+            this.ConnectionString = ConfigurationManager.ConnectionStrings[key].ConnectionString;
+        }
+        public abstract DbProviderFactory DbProviderFactory { get; }
+
+        public string ConnectionString { get; }
+
+        private T Execute<T>(Func<DbCommand, T> func, string sql, CommandType commandType, params DbParameter[] parameters)
+        {
+            try
+            {
+                using (DbConnection conn = DbProviderFactory.CreateConnection())
+                {
+                    conn.ConnectionString = ConnectionString;
+                    if (conn.State != ConnectionState.Open) conn.Open();
+                    using (DbCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        cmd.CommandType = commandType;
+                        if (parameters != null && parameters.Length > 0)
+                            cmd.Parameters.AddRange(parameters);
+                        return func.Invoke(cmd);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int ExecuteNonQuery(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<int>(cmd => cmd.ExecuteNonQuery(), sql, commandType, parameters);
+        public object ExecuteScalar(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<object>(cmd => cmd.ExecuteScalar(), sql, commandType, parameters);
+        public DbDataReader ExecuteReader(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<DbDataReader>(cmd => cmd.ExecuteReader(), sql, commandType, parameters);
+        public DataTable ExecuteAdapter(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<DataTable>(cmd =>
+       {
+           DataTable dt = new DataTable();
+           DbProviderFactory.CreateDataAdapter().Fill(dt);
+           return dt.Rows.Count <= 0 ? null : dt;
+       }
+        , sql, commandType, parameters);
+
+
+        private T ExecuteInTran<T>(Func<DbCommand, T> func, Func<T, bool> isRollbackFunc, string sql, CommandType commandType, params DbParameter[] parameters)
+        {
+            DbTransaction transaction = null;
+            try
+            {
+                using (DbConnection conn = DbProviderFactory.CreateConnection())
+                {
+                    conn.ConnectionString = this.ConnectionString;
+                    if (conn.State != ConnectionState.Open) conn.Open();
+                    using (DbCommand cmd = conn.CreateCommand())
+                    {
+                        transaction = conn.BeginTransaction();
+
+                        cmd.CommandText = sql;
+                        cmd.CommandType = commandType;
+                        if (parameters != null && parameters.Length > 0) cmd.Parameters.AddRange(parameters);
+                        T t = func.Invoke(cmd);
+
+                        if (isRollbackFunc.Invoke(t))
+                        {
+                            transaction.Commit();
+                        }
+                        else
+                        {
+                            transaction.Rollback();
+                        }
+                        return t;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        public int ExecuteNonQueryInTran(string sql, Func<int, bool> isRollbackFunc, CommandType commandType = CommandType.Text, params DbParameter[] parameters)
+        {
+            return ExecuteInTran<int>(cmd => cmd.ExecuteNonQuery(), isRollbackFunc, sql, commandType, parameters);
+        }
+
+
+    }
+
+    public class SqlServerHelper : DBHelper
+    {
+        public override DbProviderFactory DbProviderFactory { get; }
+
+        public SqlServerHelper(string connectionString) : base(connectionString)
+        {
+            this.DbProviderFactory = SqlClientFactory.Instance;
+        }
+    }
+    public class OracleHelper : DBHelper
+    {
+        public override DbProviderFactory DbProviderFactory { get; }
+        public OracleHelper(string connectionString) : base(connectionString)
+        {
+            this.DbProviderFactory = OracleClientFactory.Instance;
+        }
+    }
+    //......
+
+    */
+
+    #endregion
+
+
+
+    public static class DBHelper<TProviderFactory> where TProviderFactory : DbProviderFactory
+    {
+
+        private static Dictionary<string, DbProviderFactory> _Connections = new Dictionary<string, DbProviderFactory>();
+
+
+
+        static DBHelper()
+        {
+            DbProviderFactory = (TProviderFactory)Activator.CreateInstance(typeof(TProviderFactory));
+        }
+        public static TProviderFactory DbProviderFactory;
+
+        public static string ConnectionString { get; }
+
+        private static T Execute<T>(Func<DbCommand, T> func, string sql, CommandType commandType, params DbParameter[] parameters)
+        {
+            try
+            {
+                using (DbConnection conn = DbProviderFactory.CreateConnection())
+                {
+                    conn.ConnectionString = ConnectionString;
+                    if (conn.State != ConnectionState.Open) conn.Open();
+                    using (DbCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        cmd.CommandType = commandType;
+                        if (parameters != null && parameters.Length > 0)
+                            cmd.Parameters.AddRange(parameters);
+                        return func.Invoke(cmd);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static int ExecuteNonQuery(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<int>(cmd => cmd.ExecuteNonQuery(), sql, commandType, parameters);
+        public static object ExecuteScalar(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<object>(cmd => cmd.ExecuteScalar(), sql, commandType, parameters);
+        public static DbDataReader ExecuteReader(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<DbDataReader>(cmd => cmd.ExecuteReader(), sql, commandType, parameters);
+        public static DataTable ExecuteAdapter(string sql, CommandType commandType = CommandType.Text, params DbParameter[] parameters) => Execute<DataTable>(cmd =>
+        {
+            DataTable dt = new DataTable();
+            DbProviderFactory.CreateDataAdapter().Fill(dt);
+            return dt.Rows.Count <= 0 ? null : dt;
+        }
+        , sql, commandType, parameters);
+
+
+        private static T ExecuteInTran<T>(Func<DbCommand, T> func, Func<T, bool> isRollbackFunc, string sql, CommandType commandType, params DbParameter[] parameters)
+        {
+            DbTransaction transaction = null;
+            try
+            {
+                using (DbConnection conn = DbProviderFactory.CreateConnection())
+                {
+                    conn.ConnectionString = ConnectionString;
+                    if (conn.State != ConnectionState.Open) conn.Open();
+                    using (DbCommand cmd = conn.CreateCommand())
+                    {
+                        transaction = conn.BeginTransaction();
+
+                        cmd.CommandText = sql;
+                        cmd.CommandType = commandType;
+                        if (parameters != null && parameters.Length > 0) cmd.Parameters.AddRange(parameters);
+                        T t = func.Invoke(cmd);
+
+                        if (isRollbackFunc.Invoke(t))
+                        {
+                            transaction.Commit();
+                        }
+                        else
+                        {
+                            transaction.Rollback();
+                        }
+                        return t;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        public static int ExecuteNonQueryInTran(string sql, Func<int, bool> isRollbackFunc, CommandType commandType = CommandType.Text, params DbParameter[] parameters)
+        {
+            return ExecuteInTran<int>(cmd => cmd.ExecuteNonQuery(), isRollbackFunc, sql, commandType, parameters);
+        }
+
+
+    }
+
+}
