5cec3ae82b82dedafce93a64c7964ef9f3e58444
h1213159982 <121315982@qq.com>
2021-04-24 16:06:29 +0800

    555

--- Project/HDF/WindowsFormsApp2/Program.cs
+++ Project/HDF/WindowsFormsApp2/Program.cs
@@ -80,6 +80,8 @@ namespace WindowsFormsApp2
 
 
 
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form3());
