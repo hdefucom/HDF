fd800c9f8d88a2aa3bb0d845c81971155e9227e2
德芙 <121315982@qq.com>
2021-04-24 16:14:10 +0800

    6

--- Project/HDF/WindowsFormsApp2/Program.cs
+++ Project/HDF/WindowsFormsApp2/Program.cs
@@ -82,6 +82,8 @@ namespace WindowsFormsApp2
 
 
 
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form3());
