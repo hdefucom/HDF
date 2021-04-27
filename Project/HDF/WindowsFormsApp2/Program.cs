c1c6965db1b6e50e2746dabd4d26d9c5ff7355c7
德芙 <1213159982@qq.com>
2021-04-27 13:39:07 +0800

    test

--- Project/HDF/WindowsFormsApp2/Program.cs
+++ Project/HDF/WindowsFormsApp2/Program.cs
@@ -67,29 +67,9 @@ namespace WindowsFormsApp2
 
             */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form3());
+            Application.Run(new Form4());
         }
 
 
