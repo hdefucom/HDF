aa0d7c0173b8ab53d775a32aa81cc1c14546ec28
德芙 <1213159982@qq.com>
2021-12-17 15:13:38 +0800

    1

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json.Linq;
+using Oracle.ManagedDataAccess.Client;
 using System.Reflection.Emit;
 
 namespace WinFormsApp1;
@@ -14,18 +15,52 @@ static class Program
     static unsafe void Main()
     {
 
+        string constring = "";
 
-        var t = new Test { Tag = new { Name = "h" } };
+        OracleConnection conn = new OracleConnection(constring);
+
+        conn.Open();
+
+        var command = conn.CreateCommand();
+        //command.CommandText = "select trade_price from drug_stock where trade_price=1";
+        command.CommandText = "select menu_sequ from pub_emr_catalog";
+
+        OracleDataAdapter oracleDataAdapter = new OracleDataAdapter();
+
+        oracleDataAdapter.SelectCommand = command;
+
+        DataTable dt3 = new DataTable();
+
+        oracleDataAdapter.Fill(dt3);
 
 
-        var obj2 = new Test { Tag = t.Tag };
 
-        ref var refojb2 = ref t.Tag;
+        var dt = new DataTable();
 
-        refojb2 = new { Index = 1 };
+        dt.Columns.Add("Name", typeof(string));
+        dt.Columns.Add("Num", typeof(decimal));
 
 
+        var dr1 = dt.NewRow();
+        dr1["Name"] = "123";
+        dr1["Num"] = 1.5f;
+        dt.Rows.Add(dr1);
 
+        var dr2 = dt.NewRow();
+        dr2["Name"] = "123";
+        dr2["Num"] = 1;
+        dt.Rows.Add(dr2);
+
+
+        var str1 = JsonConvert.SerializeObject(dt, new JsonSerializerSettings() { FloatFormatHandling = FloatFormatHandling.String });
+
+
+        var str2 = "[     {\"Name\":123,\"Num\":1}, {\"Name\":123,\"Num\":1.5}     ]";
+        var dt2 = JsonConvert.DeserializeObject<DataTable>(str2,
+            new JsonSerializerSettings() { FloatFormatHandling = FloatFormatHandling.DefaultValue });
+
+        var dict1 = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(str1);
+        var dict2 = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(str2);
 
 
         Application.EnableVisualStyles();
