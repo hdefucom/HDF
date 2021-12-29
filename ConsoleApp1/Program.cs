0de78947f44f605b73e7e41166f09fead20dc294
德芙 <1213159982@qq.com>
2021-12-29 17:47:02 +0800

    添加控制台项目

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+
+
+public class Test
+{
+
+    public static string GetString(string key) => $"data:{key}";
+
+
+}
