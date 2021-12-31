5673ae696168bfc4543b426c353e3a02fa17ee72
德芙 <1213159982@qq.com>
2021-12-31 16:34:49 +0800

    定时备份

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApp1;
 
@@ -6,6 +7,22 @@ internal class Program
 {
     static void Main(string[] args)
     {
+
+
+
+
+
+        //AsyncMethodBuilderAttribute
+
+
+
+
+
+
+
+
+
+
         Console.WriteLine("Hello World!");
     }
 }
--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -11,6 +11,48 @@ namespace WinFormsApp1;
 static class Program
 {
 
+
+
+    public class Test
+    {
+        public int No { get; set; }
+        public int GroupNo { get; set; }
+    }
+
+
+
+    public class TestGroupInfo
+    {
+        public int No { get; set; }
+        public List<int> GroupList { get; set; }
+    }
+
+
+    public static void Method()
+    {
+        List<Test> list = new()
+        {
+            new Test() { No = 1, GroupNo = 1 },
+            new Test() { No = 1, GroupNo = 2 },
+            new Test() { No = 1, GroupNo = 3 },
+            new Test() { No = 1, GroupNo = 4 },
+            new Test() { No = 2, GroupNo = 1 },
+            new Test() { No = 2, GroupNo = 2 },
+            new Test() { No = 2, GroupNo = 3 },
+            new Test() { No = 2, GroupNo = 4 },
+        };
+
+        var res = list.GroupBy(
+            t => t.No,//最后一个委托参数的第一个参数（key）
+            t => t.GroupNo,//最后一个委托参数的第二个参数（groupby的单组集合中的元素）
+            (no, grouplist) => new TestGroupInfo { No = no, GroupList = grouplist.ToList() }
+            ).ToList();
+
+
+    }
+
+
+
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
@@ -18,21 +60,19 @@ static class Program
     //[MTAThread]
     static unsafe void Main()
     {
+
+
+
+
+
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-
-
-
 
 
         Application.Run(new Form4());
 
 
-
     }
-
-
-
 
 
 
