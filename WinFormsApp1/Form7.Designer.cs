eadd15760347bf71c0972f746cbd4d15053c60d9
德芙 <1213159982@qq.com>
2022-04-01 20:30:06 +0800

    添加webview2测试

--- WinFormsApp1/Form7.Designer.cs
+++ WinFormsApp1/Form7.Designer.cs
@@ -33,6 +33,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,47 +50,77 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
             this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(144, 23);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(35, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(222, 120);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(366, 108);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Location = new System.Drawing.Point(84, 47);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 47);
+            this.button2.Size = new System.Drawing.Size(75, 37);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 56);
+            this.button3.Location = new System.Drawing.Point(84, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 48);
+            this.button3.Size = new System.Drawing.Size(75, 38);
             this.button3.TabIndex = 2;
             this.button3.Text = "button2";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(84, 3);
+            this.button4.Location = new System.Drawing.Point(3, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 99);
+            this.button4.Size = new System.Drawing.Size(75, 77);
             this.button4.TabIndex = 2;
             this.button4.Text = "button2";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.flowLayoutPanel1.SetFlowBreak(this.button5, true);
+            this.button5.Location = new System.Drawing.Point(165, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 37);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "button2";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(246, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 81);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "button2";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(526, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 93);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 243);
+            this.ClientSize = new System.Drawing.Size(803, 404);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.button1);
             this.Name = "Form7";
@@ -105,5 +138,8 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label1;
     }
 }
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,58 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HandleResize();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != (int)Keys.Enter)
+                return;
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+                return;
+
+
+            webView21.Source = new System.Uri(textBox1.Text);
+            //webView21.CoreWebView2.Navigate(textBox1.Text);
+            webView21.Focus();
+
+        }
+
+
+
+        private void HandleResize()
+        {
+            // Resize the webview
+            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
+
+            //// Move the Events button
+            //btnEvents.Left = this.ClientSize.Width - btnEvents.Width;
+            //// Move the Go button
+            //btnGo.Left = this.btnEvents.Left - btnGo.Size.Width;
+
+            //// Resize the URL textbox
+            //txtUrl.Width = btnGo.Left - txtUrl.Left;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
