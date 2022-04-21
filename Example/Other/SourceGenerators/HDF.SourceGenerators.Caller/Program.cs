f2517bedf13a51472f25ada8f62e77fa9ac44dc9
德芙 <1213159982@qq.com>
2022-04-21 11:21:51 +0800

    添加C# SourceGenerator示例

---
+++ Example/Other/SourceGenerators/HDF.SourceGenerators.Caller/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace HDF.SourceGenerators.Caller;
+
+
+public class Program
+{
+
+    public static void Main()
+    {
+        string filename = System.Environment.CurrentDirectory;
+
+
+
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        new SomeClassInMyCode().SomeMethodIHave();
+
+        Console.WriteLine(typeof(HelloWorld).FullName);
+
+
+        Console.ReadLine();
+
+    }
+
+
+}
+
+
+
+
+
+
+public class SomeClassInMyCode
+{
+    public void SomeMethodIHave()
+    {
+        HelloWorld.SayHello();
+
+
+
+    }
+}
+
