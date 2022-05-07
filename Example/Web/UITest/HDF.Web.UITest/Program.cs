df05fd7738b8d4939f941f37344a3e9de4cdeac1
德芙 <1213159982@qq.com>
2022-05-07 10:14:13 +0800

    添加Chromium webui自动化测试框架示例项目

---
+++ Example/Web/UITest/HDF.Web.UITest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+namespace HDF.Web.UITest
+{
+    internal class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static async Task Main()
+        {
+
+
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://www.baidu.com/");
+            await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+
+
+
+        }
+
+
+
+    }
+}
