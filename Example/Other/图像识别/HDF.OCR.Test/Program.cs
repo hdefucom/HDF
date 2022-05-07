8948cb8efa8e7615921342e6b4b8d709c6a7a658
德芙 <1213159982@qq.com>
2022-05-07 10:12:47 +0800

    添加图像识别文本示例

---
+++ Example/Other/图像识别/HDF.OCR.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using PaddleOCRSharp;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HDF.OCR.Test
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            //图像文本识别
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "*.*|*.bmp;*.jpg;*.jpeg;*.tiff;*.tiff;*.png";
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            var imagebyte = File.ReadAllBytes(ofd.FileName);
+            Bitmap bitmap = new Bitmap(new MemoryStream(imagebyte));
+
+            OCRModelConfig config = null;
+            OCRParameter oCRParameter = new OCRParameter();
+
+            using PaddleOCREngine engine = new PaddleOCREngine(config, oCRParameter);
+
+            var ocrResult = engine.DetectText(bitmap);
+
+            var str = ocrResult.Text;
+
+            Console.WriteLine(str);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
