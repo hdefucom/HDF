111480ee9cf4970c639e8abb6c4d70b5b9dc4f49
德芙 <1213159982@qq.com>
2022-05-07 10:13:11 +0800

    添加benchmarkdotnet测试项目

---
+++ Example/Other/性能测试/HDF.BenchmarkDotNet/Program.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HDF.BenchmarkDotNet;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+public class TestFor
+{
+
+    List<int> list = Enumerable.Range(0, 10000).ToList();
+
+
+
+
+    [Benchmark]
+    public void Foreach()
+    {
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    [Benchmark]
+    public void For()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+    }
+
+
+    [Benchmark]
+    public void List()
+    {
+        list.ForEach(item => Console.WriteLine(item));
+    }
+
+
+    public void Linq()
+    {
+
+
+
+    }
+
+
+}
+
