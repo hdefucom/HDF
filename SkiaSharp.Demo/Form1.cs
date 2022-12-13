a9c13b1d70f128682e69c2b34f9762a81a505e77
德芙 <1213159982@qq.com>
2022-12-13 10:15:52 +0800

    skiasharp支持连体字符

--- SkiaSharp.Demo/Form1.cs
+++ SkiaSharp.Demo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using SkiaSharp.HarfBuzz;
+using System.Windows.Forms;
 
 namespace SkiaSharp.Demo
 {
@@ -22,22 +23,31 @@ namespace SkiaSharp.Demo
 
             //skia支持连体字符绘制，在wpf以及Chrom等程序中支持，
             //但是此处绘制不出来，不知道需要设置什么参数
+            //解决方案：安装SkiaSharp.HarfBuzz包，使用DrawShapedText提供支持
+
 
             SKPaint text_paint = new SKPaint
             {
                 //FakeBoldText = true,
-                TextSize = 50,
+                TextSize = 24,
                 Color = SKColors.Black,
-
-
-
 
                 //TextAlign = SKTextAlign.Center,
                 //宋体，不然不支持中文绘制;
                 //Typeface = SKTypeface.FromFamilyName("微软雅黑"),
-                Typeface = SKTypeface.FromFamilyName("Fira Code"),
-                //IsAntialias = true,
+                //Typeface = SKTypeface.FromFamilyName("Fira Code"),
+                Typeface = SKTypeface.FromFamilyName("Fira Code",
+                                                SKFontStyleWeight.Normal,
+                                                SKFontStyleWidth.Normal,
+                                                SKFontStyleSlant.Upright),
+                IsAntialias = true,
 
+            };
+            var font = new SKFont
+            {
+                Edging = SKFontEdging.SubpixelAntialias,
+                Size = 24,
+                Subpixel = true,
             };
 
             var res = text_paint.ToFont().ContainsGlyphs("==");
@@ -46,7 +56,10 @@ namespace SkiaSharp.Demo
             //逆时针旋转45度绘制
             //canvas.RotateDegrees(-45, 250, 250);
             //canvas.DrawText("h啊速度hi发sad😂😂😂", new SKPoint(250, 250), text_paint);
-            canvas.DrawText($"{res}==> => == ===", new SKPoint(250, 250), text_paint);
+            canvas.DrawText($"{res}==> => == ===", new SKPoint(250, 50), text_paint);
+            canvas.DrawText($"{res}==> => == ===", 250, 150, font, text_paint);
+            canvas.DrawShapedText($"{res}==> => == ===", 250, 250, text_paint);
+
             //canvas.Restore();
 
 
