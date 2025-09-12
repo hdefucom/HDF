dcb9582df054a6950b7b928f2c2a883f11f2d1b7
德芙 <1213159982@qq.com>
2025-09-12 08:50:11 +0800

    2.0病历数据提取工具优化

--- WindowsFormsApp1/Test1.cs
+++ WindowsFormsApp1/Test1.cs
@@ -3,7 +3,6 @@ using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
@@ -20,7 +19,7 @@ namespace WindowsFormsApp1
 
         public static void Start(string sql)
         {
-            //var xml11 = File.ReadAllText("C:\\Users\\12131\\Desktop\\5.xml");
+            //var xml11 = File.ReadAllText("C:\\Users\\12131\\Desktop\\2.xml");
             ////xml11 = xml11.GZipDecompressString(Encoding.UTF8);
 
             //var list22 = Handler(xml11);
@@ -167,7 +166,14 @@ values (:MEDICALRECORDID, :NOOFINPAT, :REUSETYPE, :REUSEVALUE1, :CREATETIME, :RE
                     var eletitle = xmlElement.InnerText.TrimEnd();
                     if (eletitle.EndsWith(":") || eletitle.EndsWith("："))
                     {
-                        outname = eletitle.TrimEnd(':','：');
+                        outname = eletitle.TrimEnd(':', '：');
+                        return true;
+                    }
+
+                    if (xmlElement.NextSibling is XmlElement { Name: "span" } xmlele222
+                        && (xmlele222.InnerText.StartsWith(":") || xmlele222.InnerText.StartsWith("：")))
+                    {
+                        outname = eletitle.TrimEnd(':', '：');
                         return true;
                     }
 
@@ -213,6 +219,48 @@ values (:MEDICALRECORDID, :NOOFINPAT, :REUSETYPE, :REUSEVALUE1, :CREATETIME, :RE
 
                         }
                     }
+                }
+                else if (item is XmlElement { Name: "table" } ele_table)
+                {
+                    var p_list = ele_table.SelectNodes(".//p");
+
+                    foreach (XmlElement p_item in p_list)
+                    {
+
+                        foreach (var item2 in p_item.ChildNodes)
+                        {
+                            if (item2 is XmlElement ele2)
+                            {
+                                if (ele2.Name == "roelement")
+                                {
+                                    //AddCurrentItem(ele2.GetAttribute("name"));
+                                    AddCurrentItem(ele2.InnerText);
+                                    continue;
+                                }
+                                else if (IsFormItemTitle(ele2, out var outname1))
+                                {
+                                    AddCurrentItem(outname1);
+                                    continue;
+                                }
+
+                                //匹配到roelement元素项后开始附加文本内容
+                                if (string.IsNullOrEmpty(current_item))
+                                    continue;
+
+                                if (ele2.Name is "span" or "macro" or "ftimeelement")
+                                {
+                                    current_item_value += ele2.InnerText.Trim();
+                                }
+                                else if (ele2.Name is "br" or "eof")
+                                {
+                                    current_item_value += Environment.NewLine;
+                                }
+
+                            }
+                        }
+                    }
+
+                    AddCurrentItem("");
                 }
 
             }
